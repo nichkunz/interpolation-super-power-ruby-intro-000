be85b5cb9c9d38fb5c6da9ee33ae940e0c129713
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-28 16:41:03 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
-def display_rainbow(colors)
-  print "#{colors.first}: #{colors}, "
+def display_rainbow(colors[a])
+  print "#{colors[a].first}: #{colors[a]}, "
 end
